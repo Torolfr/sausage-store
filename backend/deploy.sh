2d7c23866671b1aa61afebac730a58124606799c
Sergey Danilov <torolfr@yandex.ru>
2023-12-23 16:26:56 +0300

    Backend Service deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 set -xe
-curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o sausage-store-$VERSION.jar $NEXUS_REPO_URL/$NEXUS_REPO_BACKEND_NAME/com/yandex/practicum/devops/sausage-store/$VERSION/sausage-store-$VERSION.jar
+sudo cp -rf backend.service /etc/systemd/system/backend.service
+curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o sausage-store.jar $NEXUS_REPO_URL/$NEXUS_REPO_BACKEND_NAME/com/yandex/practicum/devops/sausage-store/$VERSION/sausage-store-$VERSION.jar
+sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true
+sudo systemctl daemon-reload
+sudo systemctl restart backend.service
