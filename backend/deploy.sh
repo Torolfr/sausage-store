f66133cab6d15ecd04e46f75443854412d4d5625
Sergey Danilov <torolfr@yandex.ru>
2023-12-24 15:44:12 +0300

    Correct sintax

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 set -xe
 sudo cp -rf ./backend.service /etc/systemd/system/backend.service
-curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o sausage-store.jar $NEXUS_REPO_URL/$NEXUS_REPO_BACKEND_NAME/com/yandex/practicum/devops/sausage-store/$VERSION/sausage-store-$VERSION.jar
+CURL_URL="$NEXUS_REPO_URL/$NEXUS_REPO_BACKEND_NAME/com/yandex/practicum/devops/sausage-store/$VERSION/sausage-store-$VERSION.jar"
+curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o sausage-store.jar $CURL_URL
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true
 sudo systemctl daemon-reload
 sudo systemctl restart backend.service
