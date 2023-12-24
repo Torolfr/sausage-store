e8203f077cbb5ea1cb1f93891d4d89ea097bced3
Sergey Danilov <torolfr@yandex.ru>
2023-12-24 20:42:48 +0300

    Frontend Service deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 sudo cp -rf ./frontend.service /etc/systemd/system/frontend.service
 CURL_URL="$NEXUS_REPO_URL/$NEXUS_REPO_FRONTEND_NAME/$VERSION/sausage-store-$VERSION.tar.gz"
 curl -u $NEXUS_REPO_USER:$NEXUS_REPO_PASS -o sausage-store.tar.gz $CURL_URL
-PATH="/home/frontend/sausage-store/static/dist"
+PATH=/home/frontend/sausage-store/static/dist
 sudo cp ./sausage-store.tar.gz $PATH||true
 sudo tar xzvf $PATH/sausage-store.tar.gz
 sudo systemctl daemon-reload
