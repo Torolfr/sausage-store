52d853e0e60c8c10fbcbbc401f1824dbb5c0e95f
Sergey Danilov <torolfr@yandex.ru>
2024-02-05 22:57:47 +0300

    first setup docker

---
+++ frontend/frontend_deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -xe
+sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage_network || true
+sudo docker rm -f sausage-frontend || true
+sudo docker run --rm -d --name sausage-frontend \
+     -p 8080:80 \
+     -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
+     --network=sausage_network \
+     --restart=always \
+     "${CI_REGISTRY_IMAGE}"/sausage-frontend:latest
