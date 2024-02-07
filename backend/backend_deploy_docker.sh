b5e662c7361f6a8ada807b23fc64406060355407
Данилов Сергей Юрьевич <sdanilov@fkr.local>
2024-02-07 15:52:40 +0300

    correct rmi pre-last image for backend

--- backend/backend_deploy_docker.sh
+++ backend/backend_deploy_docker.sh
@@ -2,8 +2,8 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
-sudo docker rmi gitlab.praktikum-services.ru:5050/std-024-29/sausage-store/sausage-backend
 sudo docker rm -f sausage-backend || true
+sudo docker rmi gitlab.praktikum-services.ru:5050/std-024-29/sausage-store/sausage-backend || true
 sudo docker run -d --name sausage-backend \
      --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
