a8d1ea82d60342dc0797bf00b9974f2929844183
Данилов Сергей Юрьевич <sdanilov@fkr.local>
2024-02-07 16:48:51 +0300

    correct docker rmi images front&back

--- backend/backend_deploy_docker.sh
+++ backend/backend_deploy_docker.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-echo ${CI_REGISTRY_IMAGE}
-echo ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
-sudo docker rmi gitlab.praktikum-services.ru:5050/std-024-29/sausage-store/sausage-backend || true
+sudo docker rmi ${CI_REGISTRY_IMAGE}/sausage-backend || true
 sudo docker run -d --name sausage-backend \
      --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
--- frontend/frontend_deploy_docker.sh
+++ frontend/frontend_deploy_docker.sh
@@ -3,6 +3,7 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
+sudo docker rmi ${CI_REGISTRY_IMAGE}/sausage-frontend || true
 sudo docker run -d --name sausage-frontend \
      -p 80:80 \
      -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
